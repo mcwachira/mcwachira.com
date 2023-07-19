79c71cdae097b181aa69bac71f6c66948e16c066
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-07-19 16:28:30 +0300

    recreating my blog using nextjs tailwind css  and typerscript

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl:'https://wwww.mcwachira.com' || 'http://localhost:3000',
-    generateRobotsTxt:true,
-    generateIndexSiteMap:false,
-}
--- next.config.js
+++ next.config.js
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify:true,
-  compiler:{
-    styledComponents:true,
-  },
-  i18n:{
-    locales:['en'],
-    defaultLocale:'en'
-  }
-}
+const nextConfig = {}
 
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled:process.env.ANALYZE === 'true',
-})
-
-//module.exports = withPlugins([bundleAnalyzer], nextConfig)
-module.exports = () => {
-
-  const plugins =[bundleAnalyzer];
-
-  const config =plugins.reduce((acc, next) => next(acc), {
-    ...nextConfig
-  })
-
-  return config;
-
-} 
+module.exports = nextConfig
