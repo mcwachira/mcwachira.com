1c3337caec7d316a361d476053f78269e093179b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-11 16:26:32 +0300

    Create CaseStudiesLayout.tsx

---
+++ src/components/CaseStudy/CaseStudiesLayout.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Work } from '@/types/work'
+import Navbar from '@/components/Header/Navbar'
+import { CaseStudyHero } from '@/components/CaseStudy/CaseStudyHero'
+import { CaseStudyDetails } from '@/components/CaseStudy/CaseStudyDetails'
+import { CaseStudyGallery } from '@/components/CaseStudy/CaseStudyGallery'
+import { CaseStudyTestimonial } from '@/components/CaseStudy/CaseStudyTestimonial'
+import { CaseStudyNavigation } from '@/components/CaseStudy/CaseStudyNavigation'
+import { Footer } from '@/components/Footer'
+
+interface caseStudiesLayoutProps{
+children:React.ReactElement,
+data:Work
+}
+
+const CaseStudiesLayout = ({data, children}: caseStudiesLayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{`${data.title} - Jane Doe`}</title>
+        <meta name="description" content={data.description} />
+      </Head>
+      <Navbar />
+      <CaseStudyHero
+        title={data.title}
+        subtitle={data.subtitle}
+        tags={data.tags}
+        coverImage={data.coverImage}
+      />
+      <CaseStudyDetails
+        client={data.client}
+        description={data.description}
+        projectDuration={data.projectDuration}
+        projectURL={data.projectURL}
+      >
+        {children}
+      </CaseStudyDetails>
+      <CaseStudyGallery images={data.images} />
+      <CaseStudyTestimonial
+        clientName={data.client.name}
+        testimonial={data.testimonial}
+      />
+      <CaseStudyNavigation navigation={data.navigation} />
+      <Footer newsletter={false} />
+    </>
+  )
+}
+
+export default CaseStudiesLayout
