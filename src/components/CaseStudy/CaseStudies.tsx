ec68284e13e4d6ec3afb0fb72f172d063fe53420
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-11 16:26:25 +0300

    Update CaseStudies.tsx

--- src/components/CaseStudy/CaseStudies.tsx
+++ src/components/CaseStudy/CaseStudies.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import clsx from 'clsx'
+import { Container } from '../Common/Container'
 
-const CaseStudies = () => {
+
+const CaseStudies = ({caseStudies, tags}) => {
   return (
-    <div>CaseStudies</div>
+      <section className="overflow-hidden bg-white py-16 sm:pt-24 lg:pt-32">
+      <Container className="">
+
+  <div className="mx-auto max-w-2xl lg:mx-0:lg:max-w-none">
+
+  <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
+            <h2 className="font-display text-5xl font-semibold text-slate-900 sm:text-6xl lg:leading-none">
+              Case Studies
+            </h2>
+
+            <p className="text-lg text-slate-700 lg:ml-auto lg:max-w-lg">
+              Dive into my diverse range of projects, showcasing my expertise in
+              software development, design, and dedication to delivering
+              exceptional results.
+            </p>
+          </div>
+  </div>
+</Container>
+
+
+      </section>
   )
 }
 
