987c782fca5374ee234a360c2bb6ae76818d7fb5
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-11 08:49:36 +0300

    Create CategoryIcons.tsx

---
+++ src/components/CategoryIcons.tsx
@@ -0,0 +1,203 @@
+export function WebDevelopmentIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <polyline points="8.333,14 1,14 1,1 23,1 23,14 20,14 "></polyline>{' '}
+          <line x1="13" y1="14" x2="19" y2="20" stroke="currentColor"></line>{' '}
+          <polygon
+            points=" 6,7 10,19 13,14 18,11 "
+            stroke="currentColor"
+          ></polygon>
+        </g>
+      </svg>
+    )
+  }
+  
+  export function TutorialIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <title>pc-play-media</title>
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path d="M3,13V5A2,2,0,0,1,5,3H19a2,2,0,0,1,2,2v8"></path>
+          <path d="M23,17a4,4,0,0,1-4,4H5a4,4,0,0,1-4-4Z"></path>
+          <polygon
+            points="10 7 10 13 15 10 10 7"
+            stroke="none"
+            fill="currentColor"
+          ></polygon>
+        </g>
+      </svg>
+    )
+  }
+  
+  export function BusinessIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path d="M6,3H3C1.895,3,1,3.895,1,5 v0c0,1.105,0.895,2,2,2"></path>{' '}
+          <polyline points=" 6,7 6,1 20,1 20,7 " stroke="currentColor"></polyline>{' '}
+          <path d="M23,7H3 C1.895,7,1,6.105,1,5v15c0,1.657,1.343,3,3,3h19V7z"></path>{' '}
+          <circle cx="17" cy="15" r="2"></circle>
+        </g>
+      </svg>
+    )
+  }
+  
+  export function ContentCreationIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <line x1="9" y1="14" x2="19" y2="14" stroke="currentColor"></line>{' '}
+          <line x1="9" y1="18" x2="19" y2="18" stroke="currentColor"></line>{' '}
+          <rect x="9" y="6" width="10" height="4" stroke="currentColor"></rect>{' '}
+          <path d="M3,23a2.006,2.006,0,0,1-2-2V13H2"></path>{' '}
+          <path d="M5,1V21a2.006,2.006,0,0,1-2,2H21a2.006,2.006,0,0,0,2-2V1Z"></path>
+        </g>
+      </svg>
+    )
+  }
+  
+  export function ConsultingIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+          {...props}
+        >
+          <line x1="14" y1="19" x2="17" y2="19" stroke="currentColor"></line>{' '}
+          <path d="M8.796,13.907l-5.825,3.51 C2.369,17.779,2,18.43,2,19.132V23h20v-3.868c0-0.703-0.369-1.354-0.971-1.715l-5.825-3.51"></path>{' '}
+          <path d="M6.003,6.796 c0.63,0.186,1.292,0.31,1.982,0.31c1.96,0,3.729-0.808,5-2.106c1.271,1.298,3.054,2.106,5.014,2.106"></path>{' '}
+          <path d="M6,7c0-3.314,2.686-6,6-6 s6,2.686,6,6c0,5-2.686,8-6,8S6,12,6,7z"></path>{' '}
+        </g>
+      </svg>
+    )
+  }
+  
+  export function BrandingIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <path
+            d="M10,11.01l8.378-8.389a2.122,2.122,0,0,1,3,0h0a2.122,2.122,0,0,1,0,3L13,14"
+            stroke="currentColor"
+          ></path>
+          <path
+            d="M7.514,11.941a3.214,3.214,0,1,1,4.545,4.545C10.8,17.74,6,18,6,18S6.26,13.2,7.514,11.941Z"
+            stroke="currentColor"
+          ></path>
+          <polyline points="12 2 2 2 2 22 22 22 22 12"></polyline>
+        </g>
+      </svg>
+    )
+  }
+  
+  export function ProductDevelopmentIcon(props) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        {...props}
+      >
+        <g
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75"
+          transform="translate(0.5 0.5)"
+          fill="none"
+          stroke="currentColor"
+        >
+          <rect x="2" y="2" width="20" height="20"></rect>
+          <polyline points="7 8 11 12 7 16" stroke="currentColor"></polyline>
+          <line x1="13" y1="8" x2="17" y2="8" stroke="currentColor"></line>
+          <line x1="13" y1="16" x2="17" y2="16" stroke="currentColor"></line>
+          <line x1="15" y1="12" x2="17" y2="12" stroke="currentColor"></line>
+        </g>
+      </svg>
+    )
+  }
+  
