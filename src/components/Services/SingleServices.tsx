a1b6a2578522811f367fe6368ea790b4716ec378
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-03-18 11:22:41 +0300

    Update SingleServices.tsx

--- src/components/Services/SingleServices.tsx
+++ src/components/Services/SingleServices.tsx
@@ -8,7 +8,7 @@ const {icon, title, paragraph} = service
   return (
     <div className="w-full  overflow-hidden rounded-2xl bg-slate-50 px-4 pb-14 pt-5 shadow-sm shadow-sky-100/50 ring-1 ring-slate-100">
     <div className="wow fadeInUp" data-wow-delay=".15s">
-      <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
+      <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md  bg-opacity-10 ">
         {icon}
       </div>
       <h3 className="mb-5 text-xl font-bold text-slate-900 dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
