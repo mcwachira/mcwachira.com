ad1aeb4876fdaa41e45f6c64c6fa1a3a8f64f64b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-03-18 07:52:18 +0300

    Create siteMetaData.ts

---
+++ src/lib/siteMetaData.ts
@@ -0,0 +1,20 @@
+// please update required information
+const siteMetadata = {
+    title: 'mcwachira.com',
+    author: 'mcwachira',
+    headerTitle: 'Next.js  website',
+    description: 'A website built using Nextjs , tailwindcss and contentlayer .',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://mcwachira.com', // your website URL
+    siteLogo: '/images/logos/png/color.png',
+    // socialBanner: '/images/coffee/social/social-banner.png', // add social banner in the public folder
+    email: 'mcwachira@outlook.com',
+    github: 'https://github.com/mcwachira',
+    twitter: 'https://twitter.com/mc_wachira',
+    // facebook: 'https://www.facebook.com/CoffeeEventsKE?mibextid=kFxxJD',
+    instagram:'https://www.instagram.com/mcwachira',
+    locale: 'en-US',
+}
+
+export default  siteMetadata
