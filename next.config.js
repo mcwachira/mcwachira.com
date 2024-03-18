4337b61d27df76dd581d6e5463f384ec62dfbd9d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-03-18 12:50:57 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
+    // reactStrictMode: true,
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
     images: {
         deviceSizes: [
             360, 414, 512, 640, 750, 828, 1080, 1200, 1536, 1920, 2048, 3840,
