edfe9c71e94a841ab7e7e213c8ea8c83f0773f49
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-24 16:48:30 +0300

    Made changes and updated the types

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -43,7 +43,8 @@ export default function BlogPost({ params }: BlogPostProps) {
 
 
   // Ensure the category is valid before accessing iconOptions
-  const CategoryIcon = post?.category && iconOptions[post?.category];
+  const CategoryIcon: FC<any> | undefined = post.category ? iconOptions[post.category] : undefined;
+
 
   return (
       <>
