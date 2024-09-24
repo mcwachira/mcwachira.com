acf4558081b1833b7f97c64536cf68e68d46461e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-24 16:07:27 +0300

    Updated the types for the category   details page

--- src/app/blog/(index)/categories/[categorySlug]/page.tsx
+++ src/app/blog/(index)/categories/[categorySlug]/page.tsx
@@ -1,35 +1,49 @@
-import {getAllCategories} from "../../../../../lib/articles";
-import {BlogGrid} from "../../../../../components/blog/BlogGrid";
-import {allPosts} from "contentlayer/generated";
+import { getAllCategories } from "@/lib/articles";
+import { BlogGrid } from "@/components/blog/BlogGrid";
+import { allPosts } from "contentlayer/generated";
+import {PostTypes} from "@/types/post";
 
 
-const parseCategory = (categorySlug) => {
-  const category = categorySlug
-    .replace(/-/g, ' ')
-    .split(' ')
-    .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
-    .join(' ')
-  return category
+
+// Type definition for metadata parameters
+interface MetadataParams {
+  params: {
+    categorySlug: string;
+  };
 }
 
+// Function to parse the category slug into a more readable format
+const parseCategory = (categorySlug: string): string => {
+  const category = categorySlug
+      .replace(/-/g, ' ')
+      .split(' ')
+      .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+      .join(' ')
+  return category;
+}
+
+// Function to generate static parameters for categories
 export const generateStaticParams = async () => {
-  const categories = await getAllCategories()
+  const categories: string[] = await getAllCategories();
   return categories.map((category) => ({
     categorySlug: category.replace(/ /g, '-').toLowerCase(),
-  }))
+  }));
 }
 
-export async function generateMetadata({ params }) {
-  const category = parseCategory(params.categorySlug)
-  return { title: category }
+// Function to generate metadata for the page
+export async function generateMetadata({ params }: MetadataParams) {
+  const category = parseCategory(params.categorySlug);
+  return { title: category };
 }
 
-export default async function BlogCategoryPage({ params }) {
-  const posts = allPosts.filter(
-    (post) => post.category === parseCategory(params.categorySlug)
-  )
+// Main component for the blog category page
+export default async function BlogCategoryPage({ params }: MetadataParams) {
+  const posts: PostTypes[] = allPosts.filter(
+      (post: PostTypes) => post.category === parseCategory(params.categorySlug)
+  );
 
-  return <BlogGrid posts={posts} />
+  return <BlogGrid posts={posts} />;
 }
 
-export const dynamicParams = false
+// Disable dynamic parameters
+export const dynamicParams = false;
