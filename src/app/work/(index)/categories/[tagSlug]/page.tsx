3737d6f4b409d5c02d81b84e238fffca3d8ddd62
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-24 17:46:35 +0300

    Update page.tsx

--- src/app/work/(index)/categories/[tagSlug]/page.tsx
+++ src/app/work/(index)/categories/[tagSlug]/page.tsx
@@ -1,33 +1,37 @@
-import {getAllTags} from "@/lib/caseStudies";
-import {CaseStudies} from "@/components/work/CaseStudies";
-import {allCaseStudies} from "contentlayer/generated";
+import { getAllTags } from "@/lib/caseStudies";
+import { CaseStudies } from "@/components/work/CaseStudies";
+import { allCaseStudies } from "contentlayer/generated";
 
-
-const parseTag = (tagSlug:string) => {
-  const tag = tagSlug
-    .replace(/-/g, ' ')
-    .split(' ')
-    .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
-    .join(' ')
-  return tag
+// Define the interface for params
+interface Params {
+  tagSlug: string;
 }
+
+const parseTag = (tagSlug: string) => {
+  const tag = tagSlug
+      .replace(/-/g, ' ')
+      .split(' ')
+      .map((w) => w[0].toUpperCase() + w.substring(1).toLowerCase())
+      .join(' ');
+  return tag;
+};
 
 export const generateStaticParams = async () => {
-  const tags = await getAllTags()
-  return tags.map((tag) => ({ tagSlug: tag.replace(/ /g, '-').toLowerCase() }))
+  const tags = await getAllTags();
+  return tags.map((tag) => ({ tagSlug: tag.replace(/ /g, '-').toLowerCase() }));
+};
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const tag = parseTag(params.tagSlug);
+  return { title: tag };
 }
 
-export async function generateMetadata({ params:any }) {
-  const tag = parseTag(params.tagSlug)
-  return { title: tag }
-}
-
-export default async function WorkCategoryPage({ params:any }) {
+export default async function WorkCategoryPage({ params }: { params: Params }) {
   const caseStudies = allCaseStudies.filter((caseStudy) =>
-    caseStudy?.tags.includes(parseTag(params?.tagSlug))
-  )
+      caseStudy?.tags.includes(parseTag(params.tagSlug))
+  );
 
-  return <CaseStudies caseStudies={caseStudies} />
+  return <CaseStudies caseStudies={caseStudies} />;
 }
 
-export const dynamicParams = false
+export const dynamicParams = false;
