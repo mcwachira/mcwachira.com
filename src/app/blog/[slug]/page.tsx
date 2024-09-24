ec283bfa518864cfaf0e714bbb2f1f94de01f5dd
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-24 16:37:20 +0300

    fixing types

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -24,11 +24,11 @@ interface GenerateMetadataParams {
 }
 
 export const generateStaticParams = async () =>
-    allPosts.map((post: PostTypes) => ({ slug: post.slug }));
+    allPosts.map((post: PostTypes) => ({ slug: post?.slug }));
 
 export async function generateMetadata({ params }: GenerateMetadataParams) {
-  const post = allPosts.find((post: PostTypes) => post.slug === params.slug);
-  return { title: post.title, description: post.description };
+  const post = allPosts.find((post: PostTypes) => post?.slug === params.slug);
+  return { title: post?.title, description: post?.description };
 }
 
 interface BlogPostProps {
@@ -38,9 +38,9 @@ interface BlogPostProps {
 }
 
 export default function BlogPost({ params }: BlogPostProps) {
-  const post = allPosts.find((post: PostTypes) => post.slug === params.slug);
-  const categorySlug = post.category.replace(/ /g, '-').toLowerCase();
-  const CategoryIcon = iconOptions[post.category];
+  const post = allPosts.find((post: PostTypes) => post?.slug === params.slug);
+  const categorySlug = post?.category.replace(/ /g, '-').toLowerCase();
+  const CategoryIcon = iconOptions[post?.category];
 
   return (
       <>
@@ -55,13 +55,13 @@ export default function BlogPost({ params }: BlogPostProps) {
                     className="group inline-flex items-center justify-center gap-3.5 text-base leading-5 tracking-wide text-sky-700 transition duration-200 ease-in-out hover:text-sky-600 sm:text-lg"
                 >
                   <CategoryIcon className="h-[18px] w-[18px] text-sky-700/90 transition duration-200 group-hover:text-sky-600 sm:h-5 sm:w-5" />
-                  {post.category}
+                  {post?.category}
                 </Link>
                 <h1 className="mt-6 text-center font-display text-4xl font-semibold leading-tight text-slate-900 sm:text-5xl sm:leading-tight">
-                  {post.title}
+                  {post?.title}
                 </h1>
                 <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-slate-700">
-                  {post.description}
+                  {post?.description}
                 </p>
                 <div className="mt-8 flex items-center justify-center gap-4 text-md text-slate-500">
                 <span className="flex items-center gap-2">
@@ -79,8 +79,8 @@ export default function BlogPost({ params }: BlogPostProps) {
                         d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
                     />
                   </svg>
-                  <time dateTime={post.date}>
-                    {format(parseISO(post.date), 'LLL d, yyyy')}
+                  <time dateTime={post?.date}>
+                    {format(parseISO(post?.date), 'LLL d, yyyy')}
                   </time>
                 </span>
                   <span className="flex items-center gap-2">
@@ -98,14 +98,14 @@ export default function BlogPost({ params }: BlogPostProps) {
                         d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                   </svg>
-                    {`${post.timeToRead} minute read`}
+                    {`${post?.timeToRead} minute read`}
                 </span>
                 </div>
                 <div className="mx-auto mt-16 w-full max-w-4xl">
                   <div className="aspect-h-9 aspect-w-16 relative block w-full overflow-hidden rounded-3xl shadow-lg shadow-sky-100/50 md:aspect-h-2 md:aspect-w-3">
                     <Image
-                        src={post.image}
-                        alt={post.title}
+                        src={post?.image}
+                        alt={post?.title}
                         fill={true}
                         className="w-full rounded-3xl bg-slate-100 object-cover"
                         sizes="(min-width: 1024px) 56rem, calc(100vw - 2.5rem)"
@@ -119,7 +119,7 @@ export default function BlogPost({ params }: BlogPostProps) {
             {/* Article Content */}
             <div className="bg-light dark:bg-dark px-4 sm:px-6 lg:px-8">
               <div className="prose prose-lg mx-auto max-w-2xl">
-                <MdxContent code={post.body.code} />
+                <MdxContent code={post?.body.code} />
               </div>
               <PostFooter />
             </div>
--- src/types/post.ts
+++ src/types/post.ts
@@ -1,13 +1,29 @@
+// export interface PostTypes {
+//     slug: string;
+//     title: string;
+//     description: string;
+//     category: string;
+//     date?: string; // or Date
+//     image: string;
+//     timeToRead: number; // or string
+//     url: string;
+//     body: {
+//         code: string; // or whatever structure your body content has
+//     };
+// }
+
+
+// src/types/post.ts
 export interface PostTypes {
     slug: string;
     title: string;
     description: string;
     category: string;
-    date?: string; // or Date
-    image: string;
-    timeToRead: number; // or string
+    date: string; // Format should match how you're storing dates
+    timeToRead: number; // Assuming this is in minutes
+    image: string; // URL of the image
     url: string;
     body: {
-        code: string; // or whatever structure your body content has
+        code: string; // This should match the type of the content in your MDX
     };
 }
