64c3b51ab8eff020734d3c174e06a804c20c6691
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-25 08:24:29 +0300

    Update ProjectSingle.tsx

--- src/components/Projects/ProjectSingle.tsx
+++ src/components/Projects/ProjectSingle.tsx
@@ -18,7 +18,7 @@ const ProjectSingle = (props:ProjectProps) => {
   return (
 	<motion.div
 			initial={{ opacity: 0 }}
-			animate={{ opacity: 1, delay: 1 }}
+			animate={{ opacity: 1}}
 			transition={{
 				ease: 'easeInOut',
 				duration: 0.7,
