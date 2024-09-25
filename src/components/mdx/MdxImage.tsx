7e7bd78b213b310aa631ff5cd1316ae5ffc23fa4
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-09-25 11:12:28 +0300

    Updated the types of the MdxImage Component and MdxContent Component

--- src/components/mdx/MdxImage.tsx
+++ src/components/mdx/MdxImage.tsx
@@ -1,14 +1,24 @@
-import Image from 'next/image'
+import Image, { ImageProps } from 'next/image';
 
-export function MdxImage({ alt, caption, ...props }) {
-  return (
-    <figure>
-      <Image className="w-full h-auto rounded-lg" {...props} alt={alt} />
-      {caption && (
-        <figcaption className="mt-3 text-sm text-center text-gray-500">
-          {caption}
-        </figcaption>
-      )}
-    </figure>
-  )
+interface MdxImageProps extends ImageProps {
+    alt: string;
+    caption?: string;
+}
+
+export function MdxImage({ alt, caption, src, ...props }: MdxImageProps) {
+    if (!src) {
+        console.error("Image src is required!");
+        return null; // Optionally render a placeholder or handle this case
+    }
+
+    return (
+        <figure>
+            <Image className="w-full h-auto rounded-lg" {...props} src={src} alt={alt} />
+            {caption && (
+                <figcaption className="mt-3 text-sm text-center text-gray-500">
+                    {caption}
+                </figcaption>
+            )}
+        </figure>
+    );
 }
--- src/components/mdx/MdxLink.tsx
+++ src/components/mdx/MdxLink.tsx
@@ -1,9 +1,15 @@
-import Link from 'next/link'
+import Link, { LinkProps } from 'next/link';
+import React, { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
-export function MdxLink({ href, ...props }) {
+interface MdxLinkProps extends LinkProps,
+        DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+  children: React.ReactNode;
+}
+
+export function MdxLink({ href, children, ...props }: MdxLinkProps) {
   return (
-    <Link href={href} {...props}>
-      {props.children}
-    </Link>
-  )
+      <Link href={href} {...props}>
+        {children}
+      </Link>
+  );
 }
